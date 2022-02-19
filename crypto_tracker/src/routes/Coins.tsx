a1f05eece8e52015dcf524c70c2ad699cc03a32e
reviday <reviday@naver.com>
2022-02-19 13:14:38 +0900

    #4.4 Route States

--- crypto_tracker/src/routes/Coins.tsx
+++ crypto_tracker/src/routes/Coins.tsx
@@ -23,9 +23,10 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -42,7 +43,13 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
-`
+`;
+
+const Img = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`;
 
 interface CoinInterface {
   id: string;
@@ -73,13 +80,25 @@ function Coins() {
         <Title>코인</Title>
       </Header>
       <CoinsList>
-        {loading
-          ? <Loader>Loading...</Loader>
-          : coins.map((coin) => (
-              <Coin key={coin.id}>
-                <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-              </Coin>
-            ))}
+        {loading ? (
+          <Loader>Loading...</Loader>
+        ) : (
+          coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))
+        )}
       </CoinsList>
     </Container>
   );
