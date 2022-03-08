a895fa53c6556745b39f3ef0a9b1a9cc12bd246f
reviday <reviday@naver.com>
2022-03-08 23:24:34 +0900

    #5.5 To Do Setup

---
+++ to-do-list/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
+import { darkTheme } from './theme';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
