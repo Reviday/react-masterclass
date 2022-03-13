72c68aea1c3eb3483b36941b23077afaef25814d
reviday <reviday@naver.com>
2022-03-13 23:54:14 +0900

    #5.11 Add To Do

--- to-do-list/src/ToDoList.tsx
+++ to-do-list/src/ToDoList.tsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { atom, useRecoilState } from 'recoil';
 
 interface IForm {
   toDo: string;
 }
 
+interface IToDo {
+  text: string;
+  id: number;
+  category: 'TO_DO' | 'DOING' | 'DONE';
+}
+
+const toDoState = atom<IToDo[]>({
+  key: 'toDo',
+  default: [],
+});
+
 function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = (data: IForm) => {
-    console.log('add to do', data.toDo);
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldTodos) => [{ text: toDo, id: Date.now(), category: 'TO_DO' }, ...oldTodos]);
     setValue('toDo', '');
   };
   return (
     <div>
+      <h1>To Dos</h1>
       <form onSubmit={handleSubmit(handleValid)}>
         <input
           {...register('toDo', {
@@ -22,91 +36,13 @@ function ToDoList() {
         />
         <button>Add</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-// interface IForm {
-//   email: string;
-//   firstName: string;
-//   lastName: string;
-//   username: string;
-//   password: string;
-//   password1: string;
-//   extraError?: string;
-// }
-
-// function ToDoList() {
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//     setError,
-//   } = useForm<IForm>({
-//     defaultValues: {
-//       email: '@naver.com',
-//     },
-//   });
-//   const onValid = (data: IForm) => {
-//     if (data.password !== data.password1) {
-//       setError('password1', { message: 'Password are not the same' }, { shouldFocus: true });
-//     }
-//     setError('extraError', { message: 'Server Offline' });
-//   };
-//   console.log(errors);
-//   return (
-//     <div>
-//       <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onValid)}>
-//         <input
-//           {...register('email', {
-//             required: 'Email is required',
-//             pattern: {
-//               value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-//               message: 'Only naver.com emails allowed',
-//             },
-//           })}
-//           placeholder="Email"
-//         />
-//         <span>{errors?.email?.message}</span>
-//         <input
-//           {...register('firstName', {
-//             required: 'write here',
-//             validate: {
-//               noRevi: (value) => (value.includes('revi') ? 'no revis allowed' : true),
-//               noNick: (value) => (value.includes('nick') ? 'no nicks allowed' : true),
-//             },
-//           })}
-//           placeholder="First Name"
-//         />
-//         <span>{errors?.firstName?.message}</span>
-//         <input {...register('lastName', { required: 'write here' })} placeholder="Last Name" />
-//         <span>{errors?.lastName?.message}</span>
-//         <input
-//           {...register('username', { required: 'write here', minLength: 10 })}
-//           placeholder="Username"
-//         />
-//         <span>{errors?.username?.message}</span>
-//         <input
-//           {...register('password', {
-//             required: 'Password is required',
-//             maxLength: {
-//               value: 5,
-//               message: 'Your password is too short.',
-//             },
-//           })}
-//           placeholder="Password"
-//         />
-//         <span>{errors?.password?.message}</span>
-//         <input
-//           {...register('password1', { required: 'write here', maxLength: 5 })}
-//           placeholder="Password1"
-//         />
-//         <span>{errors?.password1?.message}</span>
-//         <button>Add</button>
-//         <span>{errors?.extraError?.message}</span>
-//       </form>
-//     </div>
-//   );
-// }
 
 export default ToDoList;
