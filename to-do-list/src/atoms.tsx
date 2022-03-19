db0e1878860df5414df6de985de614bf9295d14d
reviday <reviday@naver.com>
2022-03-19 21:53:10 +0900

    #5.17 Selectors part Two

--- to-do-list/src/atoms.tsx
+++ to-do-list/src/atoms.tsx
@@ -6,6 +6,11 @@ export interface IToDo {
   category: 'TO_DO' | 'DOING' | 'DONE';
 }
 
+export const categoryState = atom({
+  key: 'category',
+  default: 'TO_DO',
+});
+
 export const toDoState = atom<IToDo[]>({
   key: 'toDo',
   default: [],
@@ -15,11 +20,7 @@ export const toDoSelector = selector({
   key: 'toDoSelector',
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      // 아래 코드를 더 개선할 수 있는 방법은 없을까?
-      toDos.filter((toDo) => toDo.category === 'TO_DO'),
-      toDos.filter((toDo) => toDo.category === 'DOING'),
-      toDos.filter((toDo) => toDo.category === 'DONE'),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
--- to-do-list/src/components/ToDoList.tsx
+++ to-do-list/src/components/ToDoList.tsx
@@ -1,36 +1,29 @@
-import { useRecoilValue } from 'recoil';
-import { toDoSelector } from '../atoms';
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { categoryState, toDoSelector } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
-  const [toDos, doing, done] = useRecoilValue(toDoSelector);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
-      <CreateToDo />
       <h1>To Dos</h1>
       <hr />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-
-      <h1>Doing</h1>
-      <hr />
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-
-      <h1>Done</h1>
-      <hr />
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      <select onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
+      <CreateToDo />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+      <ul></ul>
     </div>
   );
 }
