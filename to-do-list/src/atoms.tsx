cf86ff12162c86d9faa1686d971fbfdb3bb0d32a
reviday <reviday@naver.com>
2022-03-19 21:25:06 +0900

    #5.16 Selectors part One

--- to-do-list/src/atoms.tsx
+++ to-do-list/src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,17 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: 'toDo',
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: 'toDoSelector',
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      // 아래 코드를 더 개선할 수 있는 방법은 없을까?
+      toDos.filter((toDo) => toDo.category === 'TO_DO'),
+      toDos.filter((toDo) => toDo.category === 'DOING'),
+      toDos.filter((toDo) => toDo.category === 'DONE'),
+    ];
+  },
 });
--- to-do-list/src/components/ToDoList.tsx
+++ to-do-list/src/components/ToDoList.tsx
@@ -1,17 +1,33 @@
-import { useRecoilState } from 'recoil';
-import { toDoState } from '../atoms';
+import { useRecoilValue } from 'recoil';
+import { toDoSelector } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  console.log(toDos);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
-      <h1>To Dos</h1>
       <CreateToDo />
+      <h1>To Dos</h1>
+      <hr />
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+
+      <h1>Doing</h1>
+      <hr />
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+
+      <h1>Done</h1>
+      <hr />
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
