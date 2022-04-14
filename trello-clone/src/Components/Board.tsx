203ad1f754b429c1473377d0fa660435c112cc29
reviday <reviday@naver.com>
2022-04-14 23:47:45 +0900

    #6.12 Final Styles

--- trello-clone/src/Components/Board.tsx
+++ trello-clone/src/Components/Board.tsx
@@ -4,7 +4,6 @@ import DragabbleCard from './DragabbleCard';
 
 const Wrapper = styled.div`
   width: 300px;
-  padding: 20px 10px;
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
@@ -27,9 +26,10 @@ interface IAreaProps {
 
 const Area = styled.div<IAreaProps>`
   background-color: ${(props) =>
-    props.isDraggingOver ? 'pink' : props.isDraggingFromThisWith ? 'red' : 'blue'};
+    props.isDraggingOver ? '#dfe6e9' : props.isDraggingFromThisWith ? '#b2bec3' : 'transparent'};
   flex-grow: 1;
   transition: background-color 0.3s ease-in-out;
+  padding: 20px;
 `;
 
 interface IBoardProps {
