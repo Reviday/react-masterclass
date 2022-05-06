962fc27030e64486939edf9ad6fbec88bcb12997
reviday <reviday@naver.com>
2022-05-06 20:56:29 +0900

    #7.9 MotionValues part Three

--- animations/src/index.tsx
+++ animations/src/index.tsx
@@ -41,7 +41,6 @@ const GlobalStyle = createGlobalStyle`
     font-family:  'Source Sans Pro', sans-serif;
     line-height: 1.2;
     color: black;
-    background:linear-gradient(135deg,#e09,#d0e);
   }
   menu, ol, ul {  
     list-style: none;
