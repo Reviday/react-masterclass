f61d118028b19a05c22ddeaf751aa04ccea66ee9
reviday <reviday@naver.com>
2022-05-07 21:52:47 +0900

    #7.10 SVG Animation

--- animations/src/index.tsx
+++ animations/src/index.tsx
@@ -41,6 +41,7 @@ const GlobalStyle = createGlobalStyle`
     font-family:  'Source Sans Pro', sans-serif;
     line-height: 1.2;
     color: black;
+    background:linear-gradient(135deg,#e09,#d0e);
   }
   menu, ol, ul {  
     list-style: none;
