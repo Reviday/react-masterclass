d44f4c4e2d5fae045e52636fa2c2f0c3a45861ee
reviday <reviday@naver.com>
2022-05-17 23:58:07 +0900

    #8.6 Home Screen part Two

--- nomplex-clone/src/api.ts
+++ nomplex-clone/src/api.ts
@@ -1,6 +1,24 @@
 const API_KEY = '9e39572c8a9c0ba24d9f4036ca4ee14e';
-
 const BASE_PATH = 'https://api.themoviedb.org/3';
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then((response) =>
